92e052c5a9bc990b1ce9d07f1a95b2d3bf836ccf
ninjarhyme <ninjarhyme@gmail.com>
2016-09-27 23:31:09 +0800

    add smart ptr

--- Void/Test/Test.cpp
+++ Void/Test/Test.cpp
@@ -1,9 +1,11 @@
 #if true
 #include "../Void.h"
+#include <iostream>
 
+//----------------------------------------------------------------------------------------------------
 int main(int _argc, char** _argv)
 {
-	
+
 
 	return 0;
 }
