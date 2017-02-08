25e73b96c1c383ba64abfc484ec5c5b4b1812a0c
linzisheng <ninjarhyme@gmail.com>
2017-02-08 22:18:10 +0800

    update angle

--- Void/Structure/Angle/VAngle.cpp
+++ Void/Structure/Angle/VAngle.cpp
@@ -11,6 +11,8 @@ namespace Void
     
     //----------------------------------------------------------------------------------------------------
     VAngle::VAngle(const VAngle& _angle)
+        :
+        radian(_angle.radian)
     {
     }
     
@@ -18,4 +20,23 @@ namespace Void
     VAngle::~VAngle()
     {
     }
+    
+    //----------------------------------------------------------------------------------------------------
+    void VAngle::SetRadian(float _radian)
+    {
+        radian = _radian;
+    }
+    
+    //----------------------------------------------------------------------------------------------------
+    void VAngle::SetCircle(float _circle)
+    {
+        radian = _circle * V_ANGLE_2PI;
+    }
+    
+    //----------------------------------------------------------------------------------------------------
+    void VAngle::SetDegree(float _degree)
+    {
+        radian = _degree / 360.f * V_ANGLE_2PI;
+    }
+
 }
