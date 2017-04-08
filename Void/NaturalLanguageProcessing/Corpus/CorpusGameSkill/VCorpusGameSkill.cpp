fe6e672f9b35e381c4c1614fa2f3f62db5726fe5
linzisheng <ninjarhyme@gmail.com>
2017-04-09 03:10:38 +0800

    update corpus of game skill

--- Void/NaturalLanguageProcessing/Corpus/CorpusGameSkill/VCorpusGameSkill.cpp
+++ Void/NaturalLanguageProcessing/Corpus/CorpusGameSkill/VCorpusGameSkill.cpp
@@ -1,5 +1,7 @@
 #include "VCorpusGameSkill.h"
+#include "../../../Utility/Random/VRandom.h"
 #include <fstream>
+#include <vector>
 
 //----------------------------------------------------------------------------------------------------
 namespace Void
@@ -22,10 +24,27 @@ namespace Void
     }
     
     //----------------------------------------------------------------------------------------------------
-    std::vector<std::string> VCorpusGameSkill::RandomSkills(unsigned int count)
+    std::vector<std::string> VCorpusGameSkill::RandomSkills(unsigned int _count)
     {
         std::vector<std::string> result;
-        //auto maxSize = mSkillComponents[VPartOfSpeech::ADJECTIVE].size() * (mSkillComponents[VPartOfSpeech::NOUN].size() + mSkillComponents[VPartOfSpeech::VERB].size());
+        unsigned int maxPrefix = (unsigned int)mSkillComponents[VPartOfSpeech::ADJECTIVE].size();
+        unsigned int maxNSuffix = (unsigned int)mSkillComponents[VPartOfSpeech::NOUN].size();
+        unsigned int maxVSuffix = (unsigned int)mSkillComponents[VPartOfSpeech::VERB].size();
+        unsigned int maxSuffix = maxNSuffix + maxVSuffix;
+        unsigned int maxSize = maxPrefix * maxSuffix;
+        if (maxSize < _count)
+        {
+            _count = maxSize;
+        }
+        
+        VRandom random;
+        std::vector<int> indexes = random.MultipleUniqueRand(_count, 0, maxSize - 1);
+        for (int index : indexes)
+        {
+            std::string prefix = mSkillComponents[VPartOfSpeech::ADJECTIVE][index / maxSuffix];
+            std::string suffix = index % maxSuffix < maxNSuffix ? mSkillComponents[VPartOfSpeech::NOUN][index % maxSuffix] : mSkillComponents[VPartOfSpeech::VERB][index % maxSuffix - maxNSuffix];
+            result.push_back(prefix + suffix);
+        }
         
         return result;
     }
@@ -61,6 +80,10 @@ namespace Void
     {
         VCorpusGameSkill skill = VCorpusGameSkill();
         auto randomSkills = skill.RandomSkills();
+        randomSkills = skill.RandomSkills(3);
+        randomSkills = skill.RandomSkills(5);
+        randomSkills = skill.RandomSkills(15);
+        randomSkills = skill.RandomSkills(100);
         
         return;
     }
