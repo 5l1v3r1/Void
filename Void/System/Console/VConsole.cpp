6de3fb368fca2a5a16ce0754c621f35b7c71aab6
linzisheng <ninjarhyme@gmail.com>
2017-04-18 21:48:58 +0800

    update console

--- Void/System/Console/VConsole.cpp
+++ Void/System/Console/VConsole.cpp
@@ -1,6 +1,8 @@
 #include "VConsole.h"
 #include "../../Utility/Logger/VLogger.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <sstream>
 #if defined(WIN32) || defined(WIN64) || defined(_WIN32_WCE)
 #
 #else
@@ -65,6 +67,96 @@ namespace Void
     }
     
     //----------------------------------------------------------------------------------------------------
+    void VConsole::SetColor(VConsoleColor _color)
+    {
+        #if defined(WIN32) || defined(WIN64) || defined(_WIN32_WCE)
+        #
+        #else
+        switch (_color) {
+            case VConsoleColor::BLACK:
+                fputs("\033[30m", stderr);
+                break;
+            case VConsoleColor::RED:
+                fputs("\033[31m", stderr);
+                break;
+            case VConsoleColor::GREEN:
+                fputs("\033[32m", stderr);
+                break;
+            case VConsoleColor::YELLOW:
+                fputs("\033[33m", stderr);
+                break;
+            case VConsoleColor::BLUE:
+                fputs("\033[34m", stderr);
+                break;
+            case VConsoleColor::PURPLE:
+                fputs("\033[35m", stderr);
+                break;
+            case VConsoleColor::DARK_GREEN:
+                fputs("\033[36m", stderr);
+                break;
+            case VConsoleColor::WHITE:
+                fputs("\033[37m", stderr);
+                break;
+            case VConsoleColor::NONE:
+            default:
+                fputs("\033[39m", stderr);
+                break;
+        }
+        #endif
+    }
+    
+    //----------------------------------------------------------------------------------------------------
+    void VConsole::SetBackgroundColor(VConsoleBackgroundColor _color)
+    {
+        #if defined(WIN32) || defined(WIN64) || defined(_WIN32_WCE)
+        #
+        #else
+        switch (_color) {
+            case VConsoleBackgroundColor::BLACK:
+                fputs("\033[40m", stderr);
+                break;
+            case VConsoleBackgroundColor::DARK_RED:
+                fputs("\033[41m", stderr);
+                break;
+            case VConsoleBackgroundColor::GREEN:
+                fputs("\033[42m", stderr);
+                break;
+            case VConsoleBackgroundColor::YELLOW:
+                fputs("\033[43m", stderr);
+                break;
+            case VConsoleBackgroundColor::BLUE:
+                fputs("\033[44m", stderr);
+                break;
+            case VConsoleBackgroundColor::PURPLE:
+                fputs("\033[45m", stderr);
+                break;
+            case VConsoleBackgroundColor::DARK_GREEN:
+                fputs("\033[46m", stderr);
+                break;
+            case VConsoleBackgroundColor::WHITE:
+                fputs("\033[47m", stderr);
+                break;
+            case VConsoleBackgroundColor::NONE:
+            default:
+                fputs("\033[49m", stderr);
+                break;
+        }
+        #endif
+    }
+    
+    //----------------------------------------------------------------------------------------------------
+    void VConsole::Goto(unsigned short _x, unsigned short _y)
+    {
+        #if defined(WIN32) || defined(WIN64) || defined(_WIN32_WCE)
+        #
+        #else
+        std::stringstream sstream;
+        sstream << "\033[" << _x << ';' << _y << 'H';
+        fputs(sstream.str().c_str(), stderr);
+        #endif
+    }
+    
+    //----------------------------------------------------------------------------------------------------
     void VConsole::Initialize()
     {
         RefreshSize();
@@ -75,9 +167,14 @@ namespace Void
     void VConsoleTest()
     {
         VConsole console;
-        console.Clear();
+        console.SetColor(VConsoleColor::RED);
+        // console.SetBackgroundColor(VConsoleBackgroundColor::DARK_GREEN);
+        // console.Clear();
+        // console.Goto(1, 1);
         VLogger::Info("Console width: %u", console.Width());
         VLogger::Info("Console height: %u", console.Height());
+        console.SetColor();
+        console.SetBackgroundColor();
         
         return;
     }
--- Void/System/Console/VConsole.h
+++ Void/System/Console/VConsole.h
@@ -5,6 +5,36 @@
 //----------------------------------------------------------------------------------------------------
 namespace Void
 {
+    // VConsoleColor
+    //----------------------------------------------------------------------------------------------------
+    enum class VConsoleColor
+    {
+        NONE = 0, // Linux(39)
+        BLACK, // Linux(30)
+        RED, // Linux(31)
+        GREEN, // Linux(32)
+        YELLOW, // Linux(33)
+        BLUE, // Linux(34)
+        PURPLE, // Linux(35)
+        DARK_GREEN, // Linux(36)
+        WHITE,  // Linux(37)
+    };
+    
+    // VConsoleBackgroundColor
+    //----------------------------------------------------------------------------------------------------
+    enum class VConsoleBackgroundColor
+    {
+        NONE = 0, // Linux(49)
+        BLACK, // Linux(40)
+        DARK_RED, // Linux(41)
+        GREEN, // Linux(42)
+        YELLOW, // Linux(43)
+        BLUE, // Linux(44)
+        PURPLE, // Linux(45)
+        DARK_GREEN, // Linux(46)
+        WHITE,  // Linux(47)
+    };
+    
     // VConsole
     //----------------------------------------------------------------------------------------------------
     class VConsole
@@ -22,6 +52,9 @@ namespace Void
         //----------------------------------------------------------------------------------------------------
         void RefreshSize();
         void Clear();
+        void SetColor(VConsoleColor _color=VConsoleColor::NONE);
+        void SetBackgroundColor(VConsoleBackgroundColor _color=VConsoleBackgroundColor::NONE);
+        void Goto(unsigned short _x, unsigned short _y);
         
     protected:
         //----------------------------------------------------------------------------------------------------
