d26858251f1c3778d508ef0635b32b1cd668d5ac
linzisheng <ninjarhyme@gmail.com>
2017-04-27 00:01:26 +0800

    update dynamic matrix

--- Void/Structure/DynamicMatrix/VDynamicMatrix.cpp
+++ Void/Structure/DynamicMatrix/VDynamicMatrix.cpp
@@ -7,7 +7,16 @@ namespace Void
     //----------------------------------------------------------------------------------------------------
     void VDynamicMatrixTest()
     {
-        VDynamicMatrix<double> matrix(5, 6);
+        VDynamicMatrix<double> matrix0(5, 5, 2);
+        auto matrix1 = matrix0.SubMatrix(2, 2);
+        auto matrix2 = matrix1.SubMatrix(1, 1);
+        auto matrix3 = matrix2.Copy();
+        // matrix3(3, 3); // Throw
+        
+        double values[] = {1, 2, 3, 4, 4, 4, 5, 5, 6};
+        VDynamicMatrix<double> matrix4(3, 3, values, 9);
+        double determinant = matrix4.Determinant();
+        determinant = matrix0.Determinant();
         
         return;
     }
--- Void/Structure/DynamicMatrix/VDynamicMatrix.h
+++ Void/Structure/DynamicMatrix/VDynamicMatrix.h
@@ -2,7 +2,9 @@
 #ifndef _V_DYNAMICMATRIX_H_
 #define _V_DYNAMICMATRIX_H_
 
+#include "../../Memory/SmartPtr/VSmartPtr.h"
 #include <vector>
+#include <map>
 
 //----------------------------------------------------------------------------------------------------
 namespace Void
@@ -17,27 +19,53 @@ namespace Void
         //----------------------------------------------------------------------------------------------------
         inline VDynamicMatrix()
             :
-            mRow(0),
-            mColumn(0),
-            mMatrix()
+            mRows(0),
+            mColumns(0),
+            mRowRoutes(),
+            mColumnRoutes(),
+            mMatrix(nullptr)
         {
         }
         
-        inline VDynamicMatrix(const unsigned long& _row, const unsigned long& _column)
+        inline VDynamicMatrix(const unsigned long& _rows, const unsigned long& _columns)
             :
-            mRow(_row),
-            mColumn(_column),
-            mMatrix(_row, std::vector<_T>(_column))
+            mRows(_rows),
+            mColumns(_columns),
+            mRowRoutes(),
+            mColumnRoutes(),
+            mMatrix(new std::vector<std::vector<_T>>(_rows, std::vector<_T>(_columns)))
         {
+        }
+        
+        inline VDynamicMatrix(const unsigned long& _rows, const unsigned long& _columns, _T _defaultValue)
+            :
+            mRows(_rows),
+            mColumns(_columns),
+            mRowRoutes(),
+            mColumnRoutes(),
+            mMatrix(new std::vector<std::vector<_T>>(_rows, std::vector<_T>(_columns, _defaultValue)))
+        {
+        }
+        
+        inline VDynamicMatrix(const unsigned long& _rows, const unsigned long& _columns, _T* _values, const unsigned long& _count)
+            :
+            mRows(_rows),
+            mColumns(_columns),
+            mRowRoutes(),
+            mColumnRoutes(),
+            mMatrix(new std::vector<std::vector<_T>>(_rows, std::vector<_T>(_columns)))
+        {
+            SetValues(_values, _count);
         }
         
         inline VDynamicMatrix(const VDynamicMatrix& _matrix)
             :
-            mRow(_matrix.mRow),
-            mColumn(_matrix.mColumn),
+            mRows(_matrix.mRows),
+            mColumns(_matrix.mColumns),
+            mRowRoutes(_matrix.mRowRoutes),
+            mColumnRoutes(_matrix.mColumnRoutes),
             mMatrix(_matrix.mMatrix)
         {
-            
         }
         
         ~VDynamicMatrix()
@@ -45,17 +73,195 @@ namespace Void
         }
         
         //----------------------------------------------------------------------------------------------------
+        unsigned long Rows() const
+        {
+            return mRows;
+        }
+        
+        unsigned long Columns() const
+        {
+            return mColumns;
+        }
         
         //----------------------------------------------------------------------------------------------------
+        void SetValues(_T* _values, const unsigned long& _count)
+        {
+            for (unsigned long row = 0; row < mRows; ++row)
+            {
+                for (unsigned long column = 0; column < mColumns; ++column)
+                {
+                    unsigned long index = row * mColumns + column;
+                    if (_count <= index)
+                    {
+                        return;
+                    }
+                    (*this)(row, column) = _values[index];
+                }
+            }
+        }
+        
+        //----------------------------------------------------------------------------------------------------
+        _T& operator()(const unsigned long& _row, const unsigned long& _column)
+        {
+            if (_row < mRows && _column < mColumns)
+            {
+                unsigned long realRow = _row;
+                auto route = mRowRoutes.find(_row);
+                if (route != mRowRoutes.end())
+                {
+                    realRow = route->second;
+                }
+                unsigned long realColumn = _column;
+                route = mColumnRoutes.find(_column);
+                if (route != mColumnRoutes.end())
+                {
+                    realColumn = route->second;
+                }
+                return (*mMatrix)[realRow][realColumn];
+            }
+            throw "VDynamicMatrix: out of range";
+        }
+        
+        _T operator()(const unsigned long& _row, const unsigned long& _column) const
+        {
+            if (_row < mRows && _column < mColumns)
+            {
+                unsigned long realRow = _row;
+                auto route = mRowRoutes.find(_row);
+                if (route != mRowRoutes.end())
+                {
+                    realRow = route->second;
+                }
+                unsigned long realColumn = _column;
+                route = mColumnRoutes.find(_column);
+                if (route != mColumnRoutes.end())
+                {
+                    realColumn = route->second;
+                }
+                return (*mMatrix)[realRow][realColumn];
+            }
+            throw "VDynamicMatrix: out of range";
+        }
+        
+        //----------------------------------------------------------------------------------------------------
+        VDynamicMatrix& operator=(const long long& _matrix);
+        VDynamicMatrix& operator=(const VDynamicMatrix& _matrix);
+        VDynamicMatrix operator+(const VDynamicMatrix& _matrix) const;
+        VDynamicMatrix& operator+=(const VDynamicMatrix& _matrix);
+        VDynamicMatrix operator+(long long _integer) const;
+        VDynamicMatrix& operator+=(long long _integer);
+        friend VDynamicMatrix operator+(long long _integer, const VDynamicMatrix& _matrix);
+        VDynamicMatrix operator-() const;
+        VDynamicMatrix operator-(const VDynamicMatrix& _matrix) const;
+        VDynamicMatrix& operator-=(const VDynamicMatrix& _matrix);
+        VDynamicMatrix operator-(long long _integer) const;
+        VDynamicMatrix& operator-=(long long _integer);
+        friend VDynamicMatrix operator-(long long _integer, const VDynamicMatrix& _matrix);
+        VDynamicMatrix operator*(const VDynamicMatrix& _matrix) const;
+        VDynamicMatrix& operator*=(const VDynamicMatrix& _matrix);
+        VDynamicMatrix operator*(long long _integer) const;
+        VDynamicMatrix& operator*=(long long _integer);
+        friend VDynamicMatrix operator*(long long _integer, const VDynamicMatrix& _matrix);
+        VDynamicMatrix operator/(const VDynamicMatrix& _matrix) const;
+        VDynamicMatrix& operator/=(const VDynamicMatrix& _matrix);
+        VDynamicMatrix operator/(long long _integer) const;
+        VDynamicMatrix& operator/=(long long _integer);
+        friend VDynamicMatrix operator/(long long _integer, const VDynamicMatrix& _matrix);
+        
+        //----------------------------------------------------------------------------------------------------
+        VDynamicMatrix SubMatrix(const unsigned long& _omittedRow, const unsigned long& _omittedColumn) const
+        {
+            VDynamicMatrix subMatrix(*this);
+            // Shift routes
+            if (_omittedRow < subMatrix.mRows)
+            {
+                --subMatrix.mRows;
+                auto& rowRoutes = subMatrix.mRowRoutes;
+                for (unsigned long row = _omittedRow; row < subMatrix.mRows; ++row)
+                {
+                    auto route = rowRoutes.find(row + 1);
+                    route != rowRoutes.end() ? rowRoutes[row] = route->second : rowRoutes[row] = row + 1;
+                }
+            }
+            if (_omittedColumn < subMatrix.mColumns)
+            {
+                --subMatrix.mColumns;
+                auto& columnRoutes = subMatrix.mColumnRoutes;
+                for (unsigned long column = _omittedColumn; column < subMatrix.mColumns; ++column)
+                {
+                    auto route = columnRoutes.find(column + 1);
+                    route != columnRoutes.end() ? columnRoutes[column] = route->second : columnRoutes[column] = column + 1;
+                }
+            }
+            return subMatrix;
+        }
+        
+        // The only way to get a copy
+        // Reconstruct a same matrix with empty routes
+        //----------------------------------------------------------------------------------------------------
+        VDynamicMatrix Copy() const
+        {
+            unsigned long rows = this->Rows();
+            unsigned long columns =  this->Columns();
+            VDynamicMatrix matrix(rows, columns);
+            for (unsigned long row = 0; row < rows; ++row)
+            {
+                for (unsigned long column = 0; column < columns; ++column)
+                {
+                    matrix(row, column) = (*this)(row, column);
+                }
+            }
+            return matrix;
+        }
+        
+        // Minor recursive
+        //----------------------------------------------------------------------------------------------------
+        _T Determinant()
+        {
+            _T result = static_cast<_T>(0);
+            if (mRows == mColumns && mRows != 0)
+            {
+                if (mRows == 1)
+                {
+                    return (*this)(0, 0);
+                }
+                else if (mRows == 2)
+                {
+                    return (*this)(0, 0) * (*this)(1, 1) - (*this)(0, 1) * (*this)(1, 0);
+                }
+                else
+                {
+                    for (unsigned long column = 0; column < mColumns; ++column)
+                    {
+                        VDynamicMatrix subMatrix = SubMatrix(0, column);
+                        if (column % 2)
+                        {
+                            result -= (*this)(0, column) * subMatrix.Determinant();
+                        }
+                        else
+                        {
+                            result += (*this)(0, column) * subMatrix.Determinant();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // Error
+            }
+            return result;
+        }
         
     protected:
         //----------------------------------------------------------------------------------------------------
         
     protected:
         //----------------------------------------------------------------------------------------------------
-        unsigned long mRow;
-        unsigned long mColumn;
-        std::vector<std::vector<_T>> mMatrix;
+        unsigned long mRows;
+        unsigned long mColumns;
+        std::map<unsigned long, unsigned long> mRowRoutes; // empty == common index
+        std::map<unsigned long, unsigned long> mColumnRoutes; // empty == common index
+        VSmartPtr<std::vector<std::vector<_T>>> mMatrix;
     };
     
     // Test
