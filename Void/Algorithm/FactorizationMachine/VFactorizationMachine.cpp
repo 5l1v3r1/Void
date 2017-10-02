74de4dfc60a707e00dfaa13ce2ee9babf1cc3a2a
linzisheng <ninjarhyme@gmail.com>
2017-10-02 16:23:02 +0800

    update test

--- Void/Algorithm/FactorizationMachine/VFactorizationMachine.cpp
+++ Void/Algorithm/FactorizationMachine/VFactorizationMachine.cpp
@@ -17,8 +17,9 @@ namespace Void
         samples.push_back({{{0, 1.0}, {1, 4.0}, {2, 2.0}}, 2});
         samples.push_back({{{0, 1.0}, {1, 6.0}, {2, 1.0}}, 3});
         machine.Train(samples, 100);
-
         
+        double result = 0;
+        machine.Perceive({{0, 1.0}, {1, 7.0}, {2, 0.0}}, result);
         return;
     }
     
